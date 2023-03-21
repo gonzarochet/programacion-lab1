a78aabe80b8199fa7dbc5172d2f76116ef76759b
gonzarochet <rochetgonzalo@gmail.com>
2023-03-21 14:38:28 -0300

    funcion mostrar pila en vertical

--- hello-world/main.c
+++ hello-world/main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pila.h"
+
+
 
 int main()
 {
+    /*
     int num = 0;
     printf("Ingrese un numero: \n");
     scanf("%d", &num);
     printf("El numero ingresado es: %d \n", num);
+    */
+
+    Pila pilita;
+
+    inicpila(&pilita);
+
+    leer(&pilita);
+    leer(&pilita);
+    leer(&pilita);
+    leer(&pilita);
+    leer(&pilita);
+
+
+    mostrar2(&pilita);
+    mostrar(&pilita);
+
 
 
     return 0;
